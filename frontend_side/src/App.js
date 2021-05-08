4c636d44f4c4beabc704780089581f642f007d28
Laith-Grira <lgrira12@gmail.com>
2021-05-07 23:13:29 -0400

    adding the filter

--- frontend_side/src/App.js
+++ frontend_side/src/App.js
@@ -1,30 +1,65 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [searchMovie, setSearchMovie] = useState("");
+
+  const data = [
+    {
+      movie: 'Star wars',
+      date: 1978,
+    },
+    {
+      movie: 'Fast and Furious',
+      date: 2001,
+    },
+    {
+      movie: 'End Game',
+      date: 2019,
+    },
+    {
+      movie: 'Click',
+      date: 2006,
+    },
+  ];
+
   return (
     <div className="App">
       <h1>The Shoppies</h1>
       <div className="search-bar">
         <label htmlFor="search">Movie Title</label>
         <br />
-        <input type="text" placeholder="Type Movie Name Here"/>
+        <input type="text" placeholder="Type Movie Name Here" onChange={(e) => setSearchMovie(e.target.value)}/>
       </div>
       <div className="serach-results">
         <div className="results">
           <h3>Results for</h3>
-          <ul>
-            <li>result 1 <button>Nominate</button></li>
-            <li>result 2</li>
-            <li>result 3</li>
+          {
+            data
+              .filter((val) => {
+                if (searchMovie === "") {
+                  return val;
+                } else if (val.movie.includes(searchMovie)) {
+                  return val;
+                }
+              })
+              .map((res) => (
+              <ul>
+            <li>{res.movie} <button>Nominate</button></li>
           </ul>
+            ))
+          }
         </div>
         <div className="nominations">
           <h3>Nominations</h3>
-          <ul>
-            <li>nomination 1 <button>Remove</button></li>
-            <li>nomination 2</li>
-            <li>nomination 3</li>
+          {
+            data.map((res) => (
+              <ul>
+            <li>{res.movie} <button>Remove</button></li>
           </ul>
+            ))
+          }
         </div>
       </div>
     </div>
